cc422ac5e1ff3e40b619855dda295aa27c651f1a
slime7 <slime7@foxmail.com>
2025-06-28 06:03:42 +0800

    save

--- src/components/MatButton.vue
+++ src/components/MatButton.vue
@@ -35,6 +35,10 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  togglable: {
+    type: Boolean,
+    default: false,
+  },
   toggled: {
     type: Boolean,
     default: false,
@@ -47,15 +51,27 @@ const props = defineProps({
     type: String,
     default: '',
   },
-  height: {
+  /**
+   * 按钮的变体样式，可选值为 'xs' | 'sm' | 'md' | 'lg' | 'xl'。
+   * @type {'xs' | 'sm' | 'md' | 'lg' | 'xl'}
+   * @default 'sm'
+   */
+  size: {
     type: String,
-    default: '36px',
+    default: 'sm',
+  },
+  prependIcon: {
+    type: String,
+    default: '',
+  },
+  appendIcon: {
+    type: String,
+    default: '',
   },
 });
 const emit = defineEmits(['click', 'getphonenumber']);
 
 const id = `${Math.random()}`.substring(2);
-const btnHeight = computed(() => ({ '--mat-btn-height': props.height }));
 const btnClass = ['mat-button', `mat-button-id-${id}`];
 const btnColorClass = computed(() => (props.color && [
   'primary',
@@ -101,9 +117,32 @@ const btnVariantClass = computed(() => {
   }
   return variant;
 });
+const btnSizeClass = computed(() => {
+  let sizeClass = 'mat-button--size-';
+  switch (props.size) {
+  case 'xs':
+    sizeClass += 'xs';
+    break;
+  case 'md':
+    sizeClass += 'md';
+    break;
+  case 'lg':
+    sizeClass += 'lg';
+    break;
+  case 'xl':
+    sizeClass += 'xl';
+    break;
+  case 'sm':
+  default:
+    sizeClass += 'sm';
+    break;
+  }
+  return sizeClass;
+});
 const btnRoundedClass = computed(() => (props.rounded ? 'mat-button--rounded' : ''));
 const btnBlockClass = computed(() => (props.block ? 'mat-button--block' : ''));
 const btnDisabledClass = computed(() => (props.disabled ? 'mat-button--disabled' : ''));
+const btnTogglableClass = computed(() => (props.togglable ? 'mat-button--togglable' : ''));
 const btnToggledClass = computed(() => (props.toggled ? 'toggled' : ''));
 const btnIconClass = computed(() => (props.icon ? 'mat-button--icon' : ''));
 
@@ -130,7 +169,7 @@ const handlePressDown = async () => {
 const handlePressUp = async () => {
   pressedTimer = setTimeout(() => {
     pressed.value = false;
-  }, 150);
+  }, 200);
 };
 </script>
 
@@ -146,12 +185,13 @@ const handlePressUp = async () => {
       btnRoundedClass,
       btnBlockClass,
       btnDisabledClass,
+      btnTogglableClass,
       btnToggledClass,
       btnIconClass,
-      { toggled: pressed },
+      btnSizeClass,
+      { pressed },
     ]"
     :style="{
-      ...btnHeight,
       ...btnColor,
     }"
     @touchstart="handlePressDown"
@@ -160,8 +200,28 @@ const handlePressUp = async () => {
     @getphonenumber="handleGetPhoneNumber"
   >
     <div class="mat-button--content flex center">
+      <div v-if="prependIcon" class="prepend-icon flex items-center">
+        <div class="material-icons">
+          {{ prependIcon }}
+        </div>
+      </div>
+      <div class="slot-prepend-icon">
+        <slot name="prepend-icon" />
+      </div>
+
       <slot />
+
+      <div v-if="appendIcon" class="append-icon flex items-center">
+        <div class="material-icons">
+          {{ appendIcon }}
+        </div>
+      </div>
+      <div class="slot-append-icon">
+        <slot name="append-icon" />
+      </div>
     </div>
+
+    <div class="mat-button--state-layer" />
   </button>
 </template>
 
@@ -169,36 +229,118 @@ const handlePressUp = async () => {
 @reference '../assets/style/app.css';
 
 .mat-button {
-  --mat-btn-height: 36px;
   --mat-btn-color: var(--color-surface);
   --mat-btn-text-color: var(--color-on-surface);
+  --mat-btn-height: --spacing(10);
+  --mat-btn-padding: --spacing(4);
+  --mat-btn-radius: calc(var(--mat-btn-height) / 2);
+  --mat-btn-radius-square: --spacing(3);
+  --mat-btn-radius-pressed: --spacing(2);
+  --mat-btn-icon-spacing: --spacing(2);
+  --mat-btn-icon-size: --spacing(5);
+  --mat-btn-border-size: 1px;
   position: relative;
   display: inline-flex;
   align-items: center;
   justify-content: center;
   margin: 0;
-  padding: 0 16px;
-  min-width: 64px;
-  width: auto;
-  height: var(--mat-btn-height, 36px);
-  border-radius: 12px;
+  padding: 0 var(--mat-btn-padding);
+  width: initial;
+  height: var(--mat-btn-height);
+  border-radius: var(--mat-btn-radius-square);
   font-size: .875rem;
   zoom: 1;
   white-space: nowrap;
   text-decoration: none;
   touch-action: manipulation;
   user-select: none;
-  transition: border-radius .15s ease-in-out, color .15s ease-in-out, background-color .15s ease-in-out;
+  transition: all .2s cubic-bezier(.2, 0, 0, 1);
   overflow: hidden;
+
+  .mat-button--state-layer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: inherit;
+    inset: 0;
+    overflow: hidden;
+    display: flex;
+    margin: auto;
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+    pointer-events: none;
+  }
+
+  &.mat-button--rounded {
+    border-radius: var(--mat-btn-radius);
+  }
+
+  &.mat-button--size-xs {
+    --mat-btn-height: --spacing(8);
+    --mat-btn-padding: --spacing(3);
+    --mat-btn-icon-spacing: --spacing(1);
+    --mat-btn-icon-size: --spacing(5);
+  }
+
+  &.mat-button--size-md {
+    --mat-btn-height: --spacing(14);
+    --mat-btn-padding: --spacing(6);
+    --mat-btn-radius-square: --spacing(4);
+    --mat-btn-radius-pressed: --spacing(3);
+    --mat-btn-icon-size: --spacing(6);
+    font-size: 1rem;
+  }
+
+  &.mat-button--size-lg {
+    --mat-btn-height: --spacing(14);
+    --mat-btn-padding: --spacing(6);
+    --mat-btn-radius-square: --spacing(7);
+    --mat-btn-radius-pressed: --spacing(4);
+    --mat-btn-icon-spacing: --spacing(3);
+    --mat-btn-icon-size: --spacing(8);
+    --mat-btn-border-size: 2px;
+    font-size: 1.5rem;
+  }
+
+  &.mat-button--size-xl {
+    --mat-btn-height: --spacing(34);
+    --mat-btn-padding: --spacing(16);
+    --mat-btn-radius-square: --spacing(7);
+    --mat-btn-radius-pressed: --spacing(4);
+    --mat-btn-icon-spacing: --spacing(4);
+    --mat-btn-icon-size: --spacing(10);
+    --mat-btn-border-size: 3px;
+    font-size: 2rem;
+  }
 
   &.mat-button-variant-filled {
     color: var(--color-on-primary);
     background-color: var(--color-primary);
 
-    &.toggled {
-      border-radius: 12px;
-      color: var(--color-on-primary);
-      background-color: var(--color-primary);
+    &.mat-button--disabled {
+      color: --alpha(var(--color-on-surface) / .38);
+      background-color: --alpha(var(--color-on-surface) / .1);
+    }
+
+    &.mat-button--togglable {
+      color: var(--color-on-surface);
+      background-color: var(--color-surface-container);
+
+      &.toggled {
+        color: var(--color-on-primary);
+        background-color: var(--color-primary);
+        border-radius: var(--mat-btn-radius-square);
+      }
+
+      &.pressed {
+        border-radius: var(--mat-btn-radius-pressed);
+      }
+
+      &.mat-button--disabled {
+        color: --alpha(var(--color-on-surface) / .38);
+        background-color: --alpha(var(--color-on-surface) / .1);
+      }
     }
   }
 
@@ -207,39 +349,115 @@ const handlePressUp = async () => {
     background-color: var(--color-surface-container-low);
     box-shadow: 0 3px 1px -2px rgba(0, 0, 0, .2), 0 2px 2px rgba(0, 0, 0, .14), 0 1px 5px rgba(0, 0, 0, .12);
 
-    &.toggled {
-      border-radius: 12px;
-      color: var(--color-on-primary);
-      background-color: var(--color-primary);
+    &.mat-button--disabled {
+      color: --alpha(var(--color-on-surface) / .38);
+      background-color: --alpha(var(--color-on-surface) / .1);
+      box-shadow: none;
+    }
+
+    &.mat-button--togglable {
+      color: var(--color-primary);
+      background-color: var(--color-surface-container-low);
+
+      &.toggled {
+        color: var(--color-on-primary);
+        background-color: var(--color-primary);
+        border-radius: var(--mat-btn-radius-square);
+      }
+
+      &.pressed {
+        border-radius: var(--mat-btn-radius-pressed);
+      }
+
+      &.mat-button--disabled {
+        color: --alpha(var(--color-on-surface) / .38);
+        background-color: --alpha(var(--color-on-surface) / .1);
+        box-shadow: none;
+      }
     }
   }
 
   &.mat-button-variant-tonal {
-    color: var(--color-on-secondary);
-    background-color: var(--color-secondary);
+    color: var(--color-on-secondary-container);
+    background-color: var(--color-secondary-container);
 
-    &.toggled {
-      border-radius: 12px;
-      color: var(--color-on-secondary);
-      background-color: var(--color-secondary);
+    &.mat-button--disabled {
+      color: --alpha(var(--color-on-surface) / .38);
+      background-color: --alpha(var(--color-on-surface) / .1);
+    }
+
+    &.mat-button--togglable {
+      color: var(--color-on-secondary-container);
+      background-color: var(--color-secondary-container);
+
+      &.toggled {
+        color: var(--color-on-secondary);
+        background-color: var(--color-secondary);
+        border-radius: var(--mat-btn-radius-square);
+      }
+
+      &.pressed {
+        border-radius: var(--mat-btn-radius-pressed);
+      }
+
+      &.mat-button--disabled {
+        color: --alpha(var(--color-on-surface) / .38);
+        background-color: --alpha(var(--color-on-surface) / .1);
+      }
     }
   }
 
   &.mat-button-variant-outlined {
-    background-color: transparent;
     color: var(--color-on-surface);
-    border: 1px solid var(--color-outline-variant);
+    background-color: transparent;
+    border: var(--mat-btn-border-size) solid var(--color-outline-variant);
 
-    &.toggled {
-      border-radius: 12px;
-      color: var(--color-inverse-on-surface);
-      background-color: var(--color-inverse-surface);
+    &.mat-button--disabled {
+      color: --alpha(var(--color-on-surface) / .38);
+      background-color: --alpha(var(--color-on-surface) / .1);
+      border-color: var(--color-outline-variant);
+    }
+
+    &.mat-button--togglable {
+      color: var(--color-on-surface);
+      border-color: var(--color-outline-variant);
+
+      &.toggled {
+        color: var(--color-inverse-on-surface);
+        background-color: var(--color-inverse-surface);
+        border-color: var(--color-inverse-surface);
+        border-radius: var(--mat-btn-radius-square);
+      }
+
+      &.pressed {
+        border-radius: var(--mat-btn-radius-pressed);
+      }
+
+      &.mat-button--disabled {
+        color: --alpha(var(--color-on-surface) / .38);
+        background-color: --alpha(var(--color-on-surface) / .1);
+        border-color: --alpha(var(--color-on-surface) / .1);
+      }
     }
   }
 
   &.mat-button-variant-text {
     background-color: transparent;
-    color: var(--color-on-surface);
+    color: var(--color-primary);
+  }
+
+  &.pressed {
+    border-radius: var(--mat-btn-radius-pressed);
+
+    .mat-button--state-layer {
+      background-color: currentColor;
+      opacity: .1;
+    }
+  }
+
+  &.mat-button--disabled {
+    color: --alpha(var(--color-on-surface) / .38);
+    background-color: --alpha(var(--color-on-surface) / .1);
   }
 
   .mat-button--content {
@@ -263,10 +481,6 @@ const handlePressUp = async () => {
     display: flex;
   }
 
-  &.mat-button--rounded {
-    border-radius: calc(infinity * 1px);
-  }
-
   &.mat-button--icon {
     padding: 0;
     min-width: initial;
@@ -274,22 +488,43 @@ const handlePressUp = async () => {
     border-radius: calc(infinity * 1px);
   }
 
-  &.mat-button--disabled {
-    opacity: .1;
-  }
-
   &.custom-color {
     color: var(--mat-btn-text-color);
     background-color: var(--mat-btn-color);
 
-    &.mat-button--text,
-    &.mat-button--outlined {
+    &.mat-button-variant-text,
+    &.mat-button-variant-outlined {
       color: var(--mat-btn-color);
       background-color: transparent;
     }
 
-    &.mat-button--outlined {
-      border: 1px solid var(--mat-btn-color);
+    &.mat-button-variant-outlined {
+      border: var(--mat-btn-border-size) solid var(--mat-btn-color);
+    }
+  }
+
+  .slot-prepend-icon,
+  .prepend-icon {
+    margin-right: var(--mat-btn-icon-spacing);
+
+    &:empty {
+      margin-right: 0;
+    }
+  }
+
+  .slot-append-icon,
+  .append-icon {
+    margin-left: var(--mat-btn-icon-spacing);
+
+    &:empty {
+      margin-left: 0;
+    }
+  }
+
+  .prepend-icon,
+  .append-icon {
+    .material-icons {
+      font-size: var(--mat-btn-icon-size);
     }
   }
 }
--- src/pages/component-show-case.vue
+++ src/pages/component-show-case.vue
@@ -13,62 +13,145 @@
             normal
           </div>
 
+          <div class="text-sm mt-2 flex flex-wrap gap-2">
+            <mat-button
+              variant="outlined"
+              size="xs"
+              togglable
+              :toggled="btnState.rounded"
+              @click="btnState.rounded = !btnState.rounded"
+            >
+              圆形
+            </mat-button>
+            <mat-button
+              variant="outlined"
+              size="xs"
+              togglable
+              :toggled="btnState.togglable"
+              @click="btnState.togglable = !btnState.togglable"
+            >
+              可开关
+            </mat-button>
+            <mat-button
+              variant="outlined"
+              size="xs"
+              togglable
+              :toggled="btnState.toggled"
+              @click="btnState.toggled = !btnState.toggled"
+            >
+              开关状态
+            </mat-button>
+            <mat-button
+              variant="outlined"
+              size="xs"
+              togglable
+              :toggled="btnState.disabled"
+              @click="btnState.disabled = !btnState.disabled"
+            >
+              禁用
+            </mat-button>
+          </div>
+
           <div class="mt-2 flex flex-wrap gap-2">
             <mat-button
               variant="filled"
+              :rounded="btnState.rounded"
+              :togglable="btnState.togglable"
+              :toggled="btnState.toggled"
+              :disabled="btnState.disabled"
             >
               filled
             </mat-button>
             <mat-button
               variant="tonal"
+              :rounded="btnState.rounded"
+              :togglable="btnState.togglable"
+              :toggled="btnState.toggled"
+              :disabled="btnState.disabled"
             >
               tonal
             </mat-button>
             <mat-button
               variant="elevated"
+              :rounded="btnState.rounded"
+              :togglable="btnState.togglable"
+              :toggled="btnState.toggled"
+              :disabled="btnState.disabled"
             >
               elevated
             </mat-button>
             <mat-button
               variant="outlined"
+              :rounded="btnState.rounded"
+              :togglable="btnState.togglable"
+              :toggled="btnState.toggled"
+              :disabled="btnState.disabled"
             >
               outlined
             </mat-button>
             <mat-button
               variant="text"
+              :rounded="btnState.rounded"
+              :togglable="btnState.togglable"
+              :toggled="btnState.toggled"
+              :disabled="btnState.disabled"
             >
               text
             </mat-button>
           </div>
 
           <div class="text-sm mt-2">
-            toggled
+            size
+          </div>
+
+          <div class="mt-2 flex flex-col gap-2">
+            <mat-button
+              size="xs"
+              variant="outlined"
+            >
+              Xsmall 超小号
+            </mat-button>
+            <mat-button
+              size="sm"
+              variant="outlined"
+            >
+              Small 小号
+            </mat-button>
+            <mat-button
+              size="md"
+              variant="outlined"
+            >
+              Medium 中号
+            </mat-button>
+            <mat-button
+              size="lg"
+              variant="outlined"
+            >
+              Large 大号
+            </mat-button>
+            <mat-button
+              size="xl"
+              variant="outlined"
+            >
+              Xlarge 超大号
+            </mat-button>
+          </div>
+
+          <div class="text-sm mt-2">
+            icon
           </div>
 
           <div class="mt-2 flex flex-wrap gap-2">
             <mat-button
-              variant="filled"
-              toggled
+              prepend-icon="home"
             >
-              filled
+              home
             </mat-button>
             <mat-button
-              variant="tonal"
-              toggled
+              size="lg"
+              append-icon="close"
             >
-              tonal
-            </mat-button>
-            <mat-button
-              variant="elevated"
-              toggled
-            >
-              elevated
-            </mat-button>
-            <mat-button
-              variant="outlined"
-              toggled
-            >
-              outlined
+              close
             </mat-button>
           </div>
         </div>
@@ -78,9 +161,17 @@
 </template>
 
 <script setup>
+import { reactive } from 'vue';
 import { onReady } from '@dcloudio/uni-app';
 import MpPage from '@/components/MpPage.vue';
 import MatButton from '@/components/MatButton.vue';
+
+const btnState = reactive({
+  rounded: true,
+  togglable: false,
+  toggled: false,
+  disabled: false,
+});
 
 const init = () => {
 };
